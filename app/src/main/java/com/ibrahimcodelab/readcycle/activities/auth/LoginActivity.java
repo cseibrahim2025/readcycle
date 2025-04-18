1d4f212324b55ab259853e1782e573ee9447813f
MD SULAIMAN <smensulaiman007@gmail.com>
2025-04-18 16:21:20 +0900

    main dashboard page added

--- app/src/main/java/com/ibrahimcodelab/readcycle/activities/auth/LoginActivity.java
+++ app/src/main/java/com/ibrahimcodelab/readcycle/activities/auth/LoginActivity.java
@@ -7,6 +7,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ibrahimcodelab.readcycle.R;
+import com.ibrahimcodelab.readcycle.activities.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +19,11 @@ public class LoginActivity extends AppCompatActivity {
 
         findViewById(R.id.txt_dont_have_account).setOnClickListener(v -> {
             startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            finish();
+        });
+
+        findViewById(R.id.btn_login).setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();
         });
 
